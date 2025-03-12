616937a6728415ea9e7bd12d2e94ce2923ffbb64
RenatoPorfirio <renatoporfiriox@gmail.com>
2025-03-12 00:05:01 -0300

    clean

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,36 +1,24 @@
 #include "display-control.h"
 #include "button-config.h"
-#include "time-manager.h"
+#include "time-config.h"
 #include "LDE.h"
 
 #include <EEPROM.h>
 
 DisplayControl display;
-TimeManager timeManager;
+EnhancedTimeSpan *ref;
 
 double delta(double lastTime);
 
 void setup() {
   Serial.begin(9600);
   display.begin();
-  timeManager.begin();
+  setupTime();
   setupButtons();
 }
 
 void loop() {
-  static double lastTime = 0;
-  double dt = delta(lastTime);
-
-  buttons.forEach([](ClickButton* button) {
-    button->checkClick();
-  });
-
-  if (dt > 1000) {
-    lastTime += dt;
-    DateTime now = rtc.now();
-    display.clear();
-    display.printAt(timeManager.dateTimeToString(now), 0, 0);
-  }
+  ClickButton::checkAllClicks();
 }
 
 double delta(double lastTime) {
